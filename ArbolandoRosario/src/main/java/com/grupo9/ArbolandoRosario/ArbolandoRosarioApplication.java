d970bb004a5b97002d5d8c6288c67e12c5a4082e
Rodolfo Meroi <RodoMeroi@gmail.com>
2021-10-26 20:12:40 -0300

    primer commit

---
+++ ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/ArbolandoRosarioApplication.java
@@ -0,0 +1,13 @@
+package com.grupo9.ArbolandoRosario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbolandoRosarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbolandoRosarioApplication.class, args);
+	}
+
+}
---
+++ ArbolandoRosario/src/test/java/com/grupo9/ArbolandoRosario/ArbolandoRosarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo9.ArbolandoRosario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbolandoRosarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
