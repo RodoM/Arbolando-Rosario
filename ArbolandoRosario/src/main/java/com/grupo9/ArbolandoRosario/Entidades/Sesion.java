6c65d04180131140bdbcaab4d1516e627744c505
Cilfo <angelcilfone@hotmail.com>
2021-10-28 19:09:12 -0300

    [back] Agregar entidad usuario & Agregar entidad sesion #14 & #12

--- ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Entidades/Sesion.java
+++ ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Entidades/Sesion.java
@@ -1,7 +1,16 @@
-
 package com.grupo9.ArbolandoRosario.Entidades;
+import java.io.Serializable;
+import javax.persistence.*;
+import lombok.Data;
 
-
-public class Sesion {
-
+@Data
+@Entity
+@Table(name = "sesion")
+public class Sesion implements Serializable{
+    private static final long serialVersionUID= 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idSesion;
+    private String mail;
+    private String contrasenha;
 }
