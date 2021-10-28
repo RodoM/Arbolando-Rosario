213161e235f3621b02b855cbfd4f2f6d4b813232
Facu <facuvillarruel.02@gmail.com>
2021-10-28 19:09:36 -0300

    [back] Entidad articulo lista

--- ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Entidades/Articulo.java
+++ ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Entidades/Articulo.java
@@ -6,15 +6,19 @@ import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import lombok.Data;
-
+import java.io.Serializable;
 
 
 @Data
 @Entity
 public class Articulo {
+
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long idArticulo;
+
     private String url_imagen;
     private Arbol arbol;
     private Usuario usuario;
