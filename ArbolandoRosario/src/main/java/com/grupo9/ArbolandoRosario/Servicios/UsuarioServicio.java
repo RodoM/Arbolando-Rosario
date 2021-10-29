567e493422f1f45d0785685df20430067d05f232
Cilfo <angelcilfone@hotmail.com>
2021-10-28 21:54:34 -0300

    [back] Agregar Servicio Usuario #23

--- ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Servicios/UsuarioServicio.java
+++ ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Servicios/UsuarioServicio.java
@@ -1,7 +1,35 @@
 
 package com.grupo9.ArbolandoRosario.Servicios;
+import com.grupo9.ArbolandoRosario.Repositorio.UsuarioDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.transaction.annotation.Transactional;
+import com.grupo9.ArbolandoRosario.Entidades.Usuario;
 
 public class UsuarioServicio {
+    @Autowired
+    private UsuarioDAO usuarioDao;
 
+    @Transactional(readOnly = true)
+    public List<Usuario> listarUsuarios(){
+        return usuarioDao.findAll();
+    }
+
+    @Transactional
+    public void guardar(Usuario usuario){
+        usuarioDao.save(usuario);
+    }
+
+    @Transactional
+    public void eliminar(Usuario usuario){
+        usuarioDao.delete(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario encontrarUsuarioPorId(Long id){
+        Usuario resultado = usuarioDao.findById(id).orElse(null);
+        return resultado;
+    }
 }
