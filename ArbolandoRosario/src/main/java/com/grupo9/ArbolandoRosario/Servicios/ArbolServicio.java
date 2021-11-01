c7e2cc90338fc79f1eeda55430e44d7f2630119b
camilaspt <camilaspitale26@gmail.com>
2021-11-01 18:10:51 -0300

    [back] Agregando servicio arbol #20

--- ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Servicios/ArbolServicio.java
+++ ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Servicios/ArbolServicio.java
@@ -1,13 +1,37 @@
 package com.grupo9.ArbolandoRosario.Servicios;
 
+import com.grupo9.ArbolandoRosario.Entidades.Arbol;
 import com.grupo9.ArbolandoRosario.Repositorio.ArbolDAO;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ArbolServicio {
 
     @Autowired
     private ArbolDAO arbolDAO;
+
+    @Transactional(readOnly = true)
+    public List<Arbol> listarArboles() {
+        return arbolDAO.findAll();
+    }
+
+    @Transactional
+    public void guardar(Arbol arbol) {
+        arbolDAO.save(arbol);
+    }
+
+    @Transactional
+    public void eliminar(Arbol arbol) {
+        arbolDAO.delete(arbol);
+    }
+
+    @Transactional(readOnly = true)
+    public Arbol encontrarArbolPorId(Long id) {
+        Arbol resultado = arbolDAO.findById(id).orElse(null);
+        return resultado;
+    }
 
 }
