9dece12d4b523a4d45ca0ef18184b5a81b6e39be
Cilfo <angelcilfone@hotmail.com>
2021-11-01 18:17:09 -0300

    [back] Agregar articulo servicio #40

--- ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Servicios/ArticuloServicio.java
+++ ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Servicios/ArticuloServicio.java
@@ -1,7 +1,32 @@
 
 package com.grupo9.ArbolandoRosario.Servicios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.grupo9.ArbolandoRosario.Repositorio.ArticuloDAO;
+import org.springframework.transaction.annotation.Transactional;
+import com.grupo9.ArbolandoRosario.Entidades.Articulo;
+import java.util.List;
 
-
+@Service
 public class ArticuloServicio {
+    @Autowired
+    private ArticuloDAO articuloDAO;
 
+    @Transactional(readOnly = true)
+    public List<Articulo> listarArticulos(){
+        return articuloDAO.findAll();
+    }
+    @Transactional
+    public void guardar(Articulo articulo){
+        articuloDAO.save(articulo);
+    }
+    @Transactional
+    public void eliminar(Articulo articulo){
+        articuloDAO.delete(articulo);
+    }
+    @Transactional(readOnly = true)
+    public Articulo encontrarArticuloPorId(Long id){
+        Articulo resultado = articuloDAO.findById(id).orElse(null);
+        return resultado;
+    }
 }
