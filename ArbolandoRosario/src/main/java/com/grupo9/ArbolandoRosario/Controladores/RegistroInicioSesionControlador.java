012031b254678741fbd558f453a9a0e32d4ea3c7
Cilfo <angelcilfone@hotmail.com>
2021-11-01 18:36:05 -0300

    [back] Agregar registroiniciosesion controlador #28

--- ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/RegistroInicioSesionControlador.java
+++ ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/RegistroInicioSesionControlador.java
@@ -1,7 +1,16 @@
-
 package com.grupo9.ArbolandoRosario.Controladores;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-
+@Controller
 public class RegistroInicioSesionControlador {
-
+    @GetMapping("/login")
+    public String login(Model model){
+        return "Incio-Sesion";
+    }
+    @GetMapping("/registro")
+    public String registro(Model model){
+        return "Registro";
+    }
 }
