639c5b6f4b14b0f0034b67380e8bd131d7fd2155
Cilfo <angelcilfone@hotmail.com>
2021-11-01 18:07:55 -0300

    [back] Agregar controlador Index #25

--- ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/IndexControlador.java
+++ ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/IndexControlador.java
@@ -1,7 +1,12 @@
-
 package com.grupo9.ArbolandoRosario.Controladores;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-
+@Controller
 public class IndexControlador {
-
+    @GetMapping("/")
+    public String inicio(Model model){
+        return "Index";
+    }
 }
