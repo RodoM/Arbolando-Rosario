76ea2f8fab4aee4233d11633f451f4f366c2bd67
Facu <facuvillarruel.02@gmail.com>
2021-11-01 18:48:56 -0300

    [back] Agregar controlador articulo #26

--- ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/ArticuloControlador.java
+++ ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/ArticuloControlador.java
@@ -1,7 +1,21 @@
 
 package com.grupo9.ArbolandoRosario.Controladores;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
+@Controller
 public class ArticuloControlador {
-
+    
+    @GetMapping("/articulo")
+    public String articulo (Model model) {
+        return "Ver_articulo";
+    }
+    
+    @GetMapping("/crear-articulo")
+    public String crear_articulo (Model model) {
+        return "Crear-articulo";
+    }
+    
 }
