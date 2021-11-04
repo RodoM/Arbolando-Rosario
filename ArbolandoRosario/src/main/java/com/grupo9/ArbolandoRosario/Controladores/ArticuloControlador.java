9c969a8bfdfe35ed92db99d4c27d93f892f7cff9
Cilfo <angelcilfone@hotmail.com>
2021-11-04 18:42:39 -0300

    [back] Agregar en los controladores[ArticuloControlador, RegistroInicioSesionControlador] los metodos PostMapping #63

--- ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/ArticuloControlador.java
+++ ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/ArticuloControlador.java
@@ -1,21 +1,30 @@
 
 package com.grupo9.ArbolandoRosario.Controladores;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import com.grupo9.ArbolandoRosario.Entidades.Articulo;
+import com.grupo9.ArbolandoRosario.Servicios.ArticuloServicio;
 
 @Controller
 public class ArticuloControlador {
-    
+    @Autowired
+    ArticuloServicio articuloServicio;
     @GetMapping("/articulo")
     public String articulo (Model model) {
         return "Ver_articulo";
     }
     
     @GetMapping("/crear-articulo")
-    public String crear_articulo (Model model) {
+    public String crear_articulo (Articulo articulo) {
         return "Crear-articulo";
+    }
+    @PostMapping("/crear-articulo")
+    public String guardarArticulo(Model model, Articulo articulo){
+        articuloServicio.guardar(articulo);
+        return "redirect:/Index";
     }
     
 }
