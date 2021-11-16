29b856a9361374419b054d840e1730b74ebaaadf
camilaspt <camilaspitale26@gmail.com>
2021-11-16 18:31:06 -0300

    Preautorize perfil

--- ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/PerfilControlador.java
+++ ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/PerfilControlador.java
@@ -1,11 +1,13 @@
 package com.grupo9.ArbolandoRosario.Controladores;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PerfilControlador {
 
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @GetMapping("/perfil")
     public String perfil() {
         return "Perfil";
