209ca2566f4592d632927af5feaddd4dd778f136
camilaspt <camilaspitale26@gmail.com>
2021-11-18 20:08:27 -0300

    Cambiar contraseña

--- ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/ConfiguracionControlador.java
+++ ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/ConfiguracionControlador.java
@@ -4,6 +4,8 @@ import com.grupo9.ArbolandoRosario.Servicios.UsuarioServicio;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,22 +17,23 @@ public class ConfiguracionControlador {
     @Autowired
     UsuarioServicio usuarioServicio;
 
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @GetMapping("/configuracion")
     public String configuracion() {
-        return "Index";
+        return "Ajustes";
     }
 
     @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @GetMapping("/cambiar-clave")
-    public String cambiarClave(@RequestParam Map<String, Object> params, @RequestParam String password, @RequestParam String newPassword, Model model) {
+    public String cambiarClave(@RequestParam String password, @RequestParam String newPassword, Model model) {
         try {
-            int idUsuario = params.get("id") != null ? Integer.valueOf(params.get("id").toString()) : 0;
-            Long id = Long.valueOf(idUsuario);
-            usuarioServicio.cambiarContraseña(usuarioServicio.encontrarUsuarioPorId(id), password, newPassword);
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            usuarioServicio.cambiarContraseña(usuarioServicio.encontrarUsuarioPorMail(auth.getName()), password, newPassword);
+            return "redirect:/";
         } catch (Exception e) {
-            model.addAttribute("error", e);
+            model.addAttribute("error", e.getMessage());
+            return "Ajustes";
         }
 
-        return "Perfil";
     }
 }
