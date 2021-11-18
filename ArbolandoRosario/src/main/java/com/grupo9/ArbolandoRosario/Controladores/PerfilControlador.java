5780ffc1f8be45e68bdb9ac99136dbf6e6ad61e1
Cilfo <angelcilfone@hotmail.com>
2021-11-18 19:13:59 -0300

    Perfil

--- ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/PerfilControlador.java
+++ ArbolandoRosario/src/main/java/com/grupo9/ArbolandoRosario/Controladores/PerfilControlador.java
@@ -1,4 +1,5 @@
 package com.grupo9.ArbolandoRosario.Controladores;
+import com.grupo9.ArbolandoRosario.Servicios.ArticuloServicio;
 import com.grupo9.ArbolandoRosario.Servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -7,19 +8,33 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+import com.grupo9.ArbolandoRosario.Entidades.Articulo;
+import com.grupo9.ArbolandoRosario.Entidades.Usuario;
 
 @Controller
 public class PerfilControlador {
 
     @Autowired
     UsuarioServicio usuarioServicio;
+    @Autowired
+    ArticuloServicio articuloServicio;
 
     @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @GetMapping("/perfil")
     public String perfil(Model model) {
         usuarioServicio.ValidacionesAvatarYAgregarAlModelo(model);
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        model.addAttribute("usuario", usuarioServicio.encontrarUsuarioPorMail(auth.getName()));
+        Usuario user = usuarioServicio.encontrarUsuarioPorMail(auth.getName());
+        List<Articulo> articulosAsociados = articuloServicio.buscarPorUsuario(user);
+        if(articulosAsociados!=null){
+            for (Articulo articulo : articulosAsociados) {
+                System.out.println(articulo.getArbol().getNombre());
+            }
+        }
+        model.addAttribute("cant", articulosAsociados.size());
+        model.addAttribute("usuario", user);
+        model.addAttribute("articulos", articulosAsociados);
         return "Perfil";
     }
 
